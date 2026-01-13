d76f7f0d510c2bf1dd99c635dc5cab5778d5e0b8
cshll <233991538+cshll@users.noreply.github.com>
2026-01-13 20:25:22 +0000

    create page base

---
+++ php/index.php
@@ -0,0 +1,61 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bus Company</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header class="site-header">
+    <div class="container header-flex">
+      <div class="logo">
+        <a href="#">Bus</a>
+      </div>
+      <nav class="main-nav">
+        <ul>
+          <li><a href="#">Test 1</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+  <main class="site-content">
+    <div class="container">
+      <h1>Test</h1>
+      <p>Test 1</p>
+    </div>
+  </main>
+
+  <footer class="site-footer">
+    <div class="container">
+      <div class="footer-grid">
+        <div class="footer-col">
+          <h3>Test</h3>
+          <p>Test 1</p>
+        </div>
+
+        <div class="footer-col">
+          <h4>Quick Links</h4>
+          <ul>
+            <li><a href="#">Test 2</a></li>
+          </ul>
+        </div>
+      
+        <div class="footer-col">
+          <h4>Contact Us</h4>
+          <p>Test 1</p>
+          <p>Test 2</p>
+        </div>
+      </div>
+      <div class="footer-bottom"</div>
+        <p>&copy; 2026 Bus Test. All rights reserved.</p>
+      </div>
+    </div>
+  </footer>
+</body>
+</html>
