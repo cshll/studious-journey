59b387b0a010f1b1144667dad51a7d89b176d25e
Uriscythe <134888181+Uriscythe@users.noreply.github.com>
2026-02-05 16:27:46 +0000

    Comments Made for map section

--- php/livemap.php
+++ php/livemap.php
@@ -7,8 +7,11 @@ require 'header.php';
 <main class="site-content">
   <div class="container">
     <div id="live-map" style="width: 100%; height: 500px; border-radius: 2%;"></div>
+    <!-- Socket.io is used to connect to the server -->
     <script src="http://localhost:3000/socket.io/socket.io.js"></script>
     <script src="start-map.js"></script> 
+    <!-- start-map.js is used to start the map -->
+   <!--OpenStreetMap is used to display the map -->
   </div> 
 </main>
 
